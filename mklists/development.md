832efe13a5c0bbb0f69e20684535c49222a76490
Tom Robison <thomas.robison+github@gmail.com>
2013-01-26 19:35:55 -0500

    Add first BDD test

---
+++ mklists/development.md
@@ -0,0 +1,16 @@
+# mklists Development
+
+## Requirements
+
+- Install mklists
+- Install lettuce
+
+## Testing
+
+Change directory to tests folder
+
+    $ cd /path/to/project/mklists/tests
+
+Run lettuce!
+
+    $ lettuce
---
+++ mklists/tests/features/configSteps.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import os
+from lettuce import *
+import mklists_dev
+
+@step(u'Given I have a Rulefile')
+def given_i_have_a_rulefile(step):
+    rulefilePresent = os.path.isfile('Rulefile')
+    assert True == rulefilePresent
+@step(u'When I run mklists-dev.py')
+def when_i_run_mklists_dev_py(step):
+    assert 'success' == mklists_dev.test()
+@step(u'Then I see the configuration variables')
+def then_i_see_the_configuration_variables(step):
+    rules = ['x']
+    assert rules == mklists_dev.main()
